8f174755e0fda81afe2bf90d83f012a7360a00af
TanaseNicoleta <nicoleta.tanase@hotmail.com>
2021-04-16 11:24:14 +0300

    Flyweight

---
+++ sem5_recuperare/src/ro/ase/csie/cts/dp/flyweight/ContextEcran.java
@@ -0,0 +1,25 @@
+package ro.ase.csie.cts.dp.flyweight;
+
+public class ContextEcran {
+    int x;
+    int y;
+    String culoareTextura;
+
+    public ContextEcran(int x, int y, String culoareTextura) {
+        this.x = x;
+        this.y = y;
+        this.culoareTextura = culoareTextura;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public String getCuloareTextura() {
+        return culoareTextura;
+    }
+}
---
+++ sem5_recuperare/src/ro/ase/csie/cts/dp/flyweight/FactoryModel3D.java
@@ -0,0 +1,23 @@
+package ro.ase.csie.cts.dp.flyweight;
+
+import java.util.HashMap;
+
+public class FactoryModel3D {
+
+    static HashMap<String, InterfataModel3D> modele = new HashMap<>();
+
+    //eager instantiation
+    static {
+        //generam toate modelele 3D - Modele flyweight
+        String[] listaModel = new String[] {"Soldat", "Copac", "Cladire"};
+        for(String model : listaModel) {
+            Model3D model3D = new Model3D(model);
+            model3D.incarcaModel3D(model+".3d");
+            modele.put(model, model3D);
+        }
+    }
+
+    public static InterfataModel3D getModel3D(String tip) {
+        return modele.get(tip);
+    }
+}
